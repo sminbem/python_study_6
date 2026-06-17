91fba46f13bb4f6e3684f8149bf83920e5697292
sminbem <sminbem@naver.com>
2026-06-17 20:08:53 +0900

    인코딩 처리 및 데이터 요약확인

--- 0616.py
+++ 0616.py
@@ -3,8 +3,8 @@ import pandas as pd
 # CSV 파일 경로 지정
 file_path = './원본/한국언론진흥재단_뉴스빅데이터_메타데이터_노인_20011231.csv'  # 파일명을 여기에 적어주세요.
 
-# CSV 파일을 읽어서 데이터프레임으로 변환
-df = pd.read_csv(file_path)
+# 한글 CSV 파일을 읽어서 데이터프레임으로 변환
+df = pd.read_csv(file_path, encoding='euc-kr')  # 인코딩이 다를 경우 'utf-8' 대신 'euc-kr' 등을 사용하세요.
 
 # 상위 5개 데이터 확인
 print(df.head())
